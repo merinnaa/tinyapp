5e44e4ad824eed799da3bf9499556257bae9a6ce
luwam <lwamyohannes2018@gmail.com>
2023-07-24 13:52:50 -0600

    _header added

--- express_server.js
+++ express_server.js
@@ -13,6 +13,14 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase };
+  res.render("urls_show", templateVars);
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
